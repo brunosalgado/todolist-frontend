a629ff829aef9335d1659bea367ee605f1a90f6a
Thaisa <thaisa.tps@gmail.com>
2019-11-01 22:53:41 -0300

    vuex

--- pages/todolist/index.js
+++ pages/todolist/index.js
@@ -2,12 +2,6 @@ import BootcampInput from '~/components/bootcamp-input/bootcamp-input.vue';
 import BootcampList from '~/components/bootcamp-list/bootcamp-list.vue';
 
 export default {
-  data() {
-    return {
-      i: 1,
-      itemsList: [],
-    }
-  },
   /**
    * registra os componentes
    */
@@ -15,17 +9,21 @@ export default {
     BootcampInput,
     BootcampList,
   },
+  computed: {
+    itemsList() {
+      return this.$store.getters['todos/TODOS']
+    }
+  },
+  mounted(){
+    this.$store.dispatch('todos/feachTodos')
+  },
   methods: {
     /**
      * adiciona item na list
      * recebe valor vindo do componente filho
      */
     addItemList(value) {
-      this.itemsList.push({
-        id: this.i++,
-        description: value,
-        checked: '1'
-      });
+      this.$store.dispatch("todos/addTodo", value)
     },
     /**
      * recebe valor vindo do componente
@@ -43,20 +41,13 @@ export default {
      * conforme action faz alteração na lista
      */
     deleteItemList(value) {
-      this.itemsList.map((i, k) => {
-        if (i._id == value.item._id) {
-          this.itemsList.splice(k, 1)
-        }
-      })
+      this.$store.dispatch("todos/removeTodo", value.item)
     },
     /**
      * muda estado de item na lista
      */
     changeStateItemList(value) {
-      let itemsList = this.itemsList.find((i) => {
-        return i._id == value.item._id
-      })
-      itemsList.checked = value.checked
+      this.$store.dispatch("todos/updateTodo", value.item)
     },
   },
 }
